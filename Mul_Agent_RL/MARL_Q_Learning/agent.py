eb4a0fbc1d1c19ab71c4cfc12b7289dd7a657d0e
CHUANG DENG NEW MAC <cdengcnc@sjtu.edu.cn>
2018-12-08 00:06:55 +0800

    write the agent.py file

--- Mul_Agent_RL/MARL_Q_Learning/agent.py
+++ Mul_Agent_RL/MARL_Q_Learning/agent.py
@@ -9,10 +9,10 @@ class agent():
         self.gamma = gamma
         self.delta = delta
         self.epsilon = epsilon
-        self.s = [] # current state
-        self.s_ = [] # next state
-        self.r = 0 # rewar
-        self.a = 0 # current Action
+        self.s = []  # current state
+        self.s_ = []  # next state
+        self.r = 0  # reward
+        self.a = 0  # current Action
         self.actions = genAction()
         self.states = genState(self.actions)
         # index =
@@ -21,16 +21,59 @@ class agent():
         
     def getActions(self):
         return self.actions
+
     def getStates(self):
         return self.states
+
     def getQTable(self):
         return self.qTable
-    
+
+    def initialStrategy(self):
+        """
+        Initialize strategy, in each states, play each action by the same probability.
+        :return:
+        """
+        initialValue = 1.0 / self.actions.shape[0]
+        initialStrategy = pd.Series([initialValue for i in range(self.states.shape[0])])
+        # for i in range(self.qTable.shape[0]):
+        #     for j in range(self.qTable.shape[1]):
+        #         self.qTable.iloc[i, j] = initialValue
+        for i in self.strategy.columns:
+            self.strategy[i] = initialStrategy
+
+    def initialQTable(self):
+        """
+        Initialize the qTable to all zeros.
+        :return:
+        """
+        initialQValue = pd.Series([0.0 for i in range(self.states.shape[0])])
+        for i in self.qTable.columns:
+            self.qTable[i] = initialQValue
+
+    def chooseAction(self, observation):
+        """
+        Choose action epsilon-greedy
+        :param observation: The states agent observed
+        :return:
+        action: the chosen action
+        """
+        state_action = self.qTable.loc[observation, :]
+        if np.random.binomial(1, self.epsilon) == 1:
+            action = np.random.choice(state_action.index, size=1)[0]
+        else:
+            action = np.random.choice(state_action.index, size=1, p=state_action.values)[0]
+        return action
+
 if __name__ == "__main__":
     A = agent(0.1, 0.2, 0.3, 0.4)
-    print (A.getActions())
-    print (A.getStates())
+    A.initialStrategy()
+    A.initialQTable()
     qTable = A.getQTable()
+    state_action = qTable.loc[0, :]
+    print (state_action.values)
+    action = np.random.choice(state_action.index, size=1, p = state_action)[0]
+    print (action)
+    print (action)
     print (qTable)
     print (qTable.loc[1, 0])
 
