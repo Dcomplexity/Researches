99faf3664b55d39171bbf375e9df3c65b09cd8e6
CHUANG DENG NEW MAC <cdengcnc@sjtu.edu.cn>
2018-12-18 11:32:30 +0800

    MARL epsilon

--- Mul_Agent_RL/MARL_Q_Learning_2/game_env.py
+++ Mul_Agent_RL/MARL_Q_Learning_2/game_env.py
@@ -1,23 +1,27 @@
-def alpha(timeStep):
-    return 1 / (10 + 0.002 * timeStep)
-
-def epsilon(timeStep): # 0.5 / (1000 + 0.000001 * self.timeStep)  #when episodes reach about 1000000, the final EPSILON is about 0.25, which means there is a big probability that it will explore
-    return 0.5 / (1 + 0.0001 * timeStep)
-
-def epsilonFixed(timeStep):
-    return 0.3
-
-
 import numpy as np
 from itertools import permutations
 
-def generateDCActions():
-    DEFECT = 0
-    COOPERATE = 1
-    actions = np.array([DEFECT, COOPERATE])
+
+def alpha_time(time_step):
+    return 1 / (10 + 0.002 * time_step)
+
+
+def epsilon_time(time_step):
+    return 0.5 / (1 + 0.0001 * time_step)
+
+
+def epsilon_fixed(time_step):
+    return 0.3
+
+
+def gen_actions():
+    defect = 0
+    cooperate = 1
+    actions = np.array([defect, cooperate])
     return actions
 
-def generateDCStates(actions):
+
+def gen_states(actions):
     states = []
     for _ in permutations(actions, 2):
         states.append(_)
@@ -26,27 +30,29 @@ def generateDCStates(actions):
     states.sort()
     return states
 
-# Create the prisoner dilemma game
-def PDGame(actionX, actionY):
-    T = 5.0
-    R = 3.0
-    P = 1.0
-    S = 0.0
-    if (actionX, actionY) == (1,1):
-        return [R,R]
-    elif (actionX, actionY) == (1, 0):
-        return [S, T]
-    elif (actionX, actionY) == (0, 1):
-        return [T, S]
-    elif (actionX, actionY) == (0, 0):
-        return [P, P]
 
-def MPGame(actionX, actionY):
-    if (actionX, actionY) == (1, 1):
-        return [1, -1]
-    elif (actionX, actionY) == (1, 0):
-        return [-1, 1]
-    elif (actionX, actionY) == (0, 1):
-        return [-1, 1]
-    elif (actionX, actionY) == (0, 0):
-        return [1, -1]
+# Create the prisoner dilemma game
+def pd_game(a_x, a_y):
+    t = 5.0
+    r = 3.0
+    p = 1.0
+    s = 0.0
+    if (a_x, a_y) == (1, 1):
+        return r, r
+    elif (a_x, a_y) == (1, 0):
+        return s, t
+    elif (a_x, a_y) == (0, 1):
+        return t, s
+    elif (a_x, a_y) == (0, 0):
+        return p, p
+
+
+def mp_game(a_x, a_y):
+    if (a_x, a_y) == (1, 1):
+        return 1, -1
+    elif (a_x, a_y) == (1, 0):
+        return -1, 1
+    elif (a_x, a_y) == (0, 1):
+        return -1, 1
+    elif (a_x, a_y) == (0, 0):
+        return 1, -1
--- Mul_Agent_RL/MARL_Q_Learning_2/pd_phc_vs_phc.py
+++ Mul_Agent_RL/MARL_Q_Learning_2/pd_phc_vs_phc.py
@@ -6,17 +6,17 @@ import pandas as pd
 from agent import *
 
 
-actions = generateDCActions()
-states = generateDCStates(actions)
+actions = gen_actions()
+states = gen_states(actions)
 
 def oneGame(actionX, actionY):
     actionX = actionX
     actionY = actionY
-    [rewardX, rewardY] = PDGame(actionX, actionY)
+    [rewardX, rewardY] = pd_game(actionX, actionY)
     nextState = (actionX, actionY)
     return rewardX, rewardY, nextState
 
-def playOneGame(agentX=agentPHC, agentY=agentPHC):
+def playOneGame(agentX=AgentPHC, agentY=AgentPHC):
     agentX = agentX
     agentY = agentY
     actionX = np.random.choice(actions)
@@ -24,14 +24,14 @@ def playOneGame(agentX=agentPHC, agentY=agentPHC):
     currentState = (actionX, actionY)
     episodes = 0
 
-    agentX.initialSelfStrategy()
-    agentX.initialActionValues()
-    agentX.initialDeltaStateAction()
-    agentX.initialDeltaStateActionTop()
-    agentY.initialSelfStrategy()
-    agentY.initialActionValues()
-    agentY.initialDeltaStateAction()
-    agentY.initialDeltaStateActionTop()
+    agentX.initial_strategy()
+    agentX.initial_q_table()
+    agentX.initial_delta()
+    agentX.initial_delta_top()
+    agentY.initial_strategy()
+    agentY.initial_q_table()
+    agentY.initial_delta()
+    agentY.initial_delta_top()
 
     strategy_history = []
 
@@ -44,64 +44,64 @@ def playOneGame(agentX=agentPHC, agentY=agentPHC):
             actionY = np.random.choice(actions)
             currentState = (actionX, actionY)
 
-        strategy_history.append([np.copy(agentX.getStrategy()), np.copy(agentY.getStrategy())])
+        strategy_history.append([np.copy(agentX.get_strategy()), np.copy(agentY.get_strategy())])
 
-        agentX.setCurrentState(currentState)
-        agentY.setCurrentState(currentState)
-        agentX.chooseAction()
-        agentY.chooseAction()
-        agentXAction = agentX.getAction()
-        agentYAction = agentY.getAction()
+        agentX.set_cur_state(currentState)
+        agentY.set_cur_state(currentState)
+        agentX.choose_action()
+        agentY.choose_action()
+        agentXAction = agentX.get_action()
+        agentYAction = agentY.get_action()
         rewardX, rewardY, nextState = oneGame(agentXAction, agentYAction)
 
-        agentX.setRewards(rewardX)
-        agentY.setRewards(rewardY) # notice the reward here, if the reward is set a rewardX, then the results will be that agent Y will choose cooperation all the time.
-        agentX.setNextState(nextState)
-        agentY.setNextState(nextState)
-        agentX.updateActionValues()
-        agentY.updateActionValues()
-        agentX.updateStrategy()
-        agentY.updateStrategy()
-        agentX.updateTimeStep()
-        agentY.updateTimeStep()
-        agentX.updateEpsilon()
-        agentY.updateEpsilon()
-        agentX.updateAlpha()
-        agentY.updateAlpha()
+        agentX.set_reward(rewardX)
+        agentY.set_reward(rewardY) # notice the reward here, if the reward is set a rewardX, then the results will be that agent Y will choose cooperation all the time.
+        agentX.set_next_state(nextState)
+        agentY.set_next_state(nextState)
+        agentX.update_q_table()
+        agentY.update_q_table()
+        agentX.update_strategy()
+        agentY.update_strategy()
+        agentX.update_time_step()
+        agentY.update_time_step()
+        agentX.update_epsilon()
+        agentY.update_epsilon()
+        agentX.update_alpha()
+        agentY.update_alpha()
 
         episodes += 1
         currentState = nextState
 
     return strategy_history
 
-def calAgentPayoffs(agentX=agentPHC, agentY=agentPHC):
-    agentX = agentX
-    agentY = agentY
-    actionX = np.random.choice(actions)
-    actionY = np.random.choice(actions)
-    currentState = (actionX, actionY)
-    episodes = 0
-    rewardXSum = 0
-    rewardYSum = 0
+# def calAgentPayoffs(agentX=agentPHC, agentY=agentPHC):
+#     agentX = agentX
+#     agentY = agentY
+#     actionX = np.random.choice(actions)
+#     actionY = np.random.choice(actions)
+#     currentState = (actionX, actionY)
+#     episodes = 0
+#     rewardXSum = 0
+#     rewardYSum = 0
+#
+#     payoffWholeEpisodes = 10e5
+#     while episodes < payoffWholeEpisodes:
+#         agentX.set_current_state(currentState)
+#         agentY.set_current_state(currentState)
+#         agentX.chooseAction()
+#         agentXAction = agentX.getAction()
+#         agentY.chooseAction()
+#         agentYAction = agentY.getAction()
+#         rewardX, rewardY, nextState = oneGame(agentXAction,agentYAction)
+#         initialEpisodes = 0
+#         rewardXSum += rewardX
+#         rewardYSum += rewardY
+#         currentState = nextState
+#         episodes += 1
+#
+#     return rewardXSum/(episodes-initialEpisodes), rewardYSum/(episodes-initialEpisodes)
 
-    payoffWholeEpisodes = 10e5
-    while episodes < payoffWholeEpisodes:
-        agentX.setCurrentState(currentState)
-        agentY.setCurrentState(currentState)
-        agentX.chooseAction()
-        agentXAction = agentX.getAction()
-        agentY.chooseAction()
-        agentYAction = agentY.getAction()
-        rewardX, rewardY, nextState = oneGame(agentXAction,agentYAction)
-        initialEpisodes = 0
-        rewardXSum += rewardX
-        rewardYSum += rewardY
-        currentState = nextState
-        episodes += 1
-
-    return rewardXSum/(episodes-initialEpisodes), rewardYSum/(episodes-initialEpisodes)
-
-def rungame(agentX=agentPHC, agentY=agentPHC):
+def rungame(agentX=AgentPHC, agentY=AgentPHC):
     agentX = agentX
     agentY = agentY
     runGameResult = playOneGame(agentX, agentY)
@@ -115,8 +115,8 @@ def pandas_result(result):
 if __name__ == "__main__":
     gammaValue = 0.99
     deltaValue = 0.0001
-    agentX = agentPHC(alpha=alpha, gamma=gammaValue, delta=deltaValue, epsilon=epsilon)
-    agentY = agentPHC(alpha=alpha, gamma=gammaValue, delta=deltaValue, epsilon=epsilon)
+    agentX = AgentPHC(gamma=gammaValue, delta=deltaValue)
+    agentY = AgentPHC(gamma=gammaValue, delta=deltaValue)
 
     pool = multiprocessing.Pool(processes=4)
     agentStrategyList = []
@@ -127,6 +127,6 @@ if __name__ == "__main__":
 
     #myfile = open('../files/results/pd_phc_vs_phc.txt', 'w')
     for res in agentStrategyList:
-        print(pandas_result(res.get()[-1]))
+        print(res.get()[-1])
         #myfile.write(str(res.get()[-1]) + '\n')
     #myfile.close()
