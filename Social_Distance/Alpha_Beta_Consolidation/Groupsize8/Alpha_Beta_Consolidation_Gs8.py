64d48a457f72df68c38a55f4d2a9d15ba2ecc914
CHUANG DENG CNC Linux <cdengcnc@sjtu.edu.cn>
2018-12-20 22:59:37 +0800

    adjust parameters

--- Social_Distance/Alpha_Beta_Consolidation/Groupsize8/Alpha_Beta_Consolidation_Gs8.py
+++ Social_Distance/Alpha_Beta_Consolidation/Groupsize8/Alpha_Beta_Consolidation_Gs8.py
@@ -261,9 +261,9 @@ if __name__ == "__main__":
     f = open(filename, 'w')
 
     startTime = datetime.datetime.now()
-    runtime = 3    
-    sampletime = 2
-    rounds = 2
+    runtime = 100    
+    sampletime = 20
+    rounds = 5
     buildResults = []
     for buildAlpha in range(-3, 4):
         for buildBeta in range(-3, 4):
