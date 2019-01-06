517e9de765d16c17291e8c94f14962b47503d4fb
CHUANG DENG CNC PC <cdengcnc@sjtu.edu.cn>
2019-01-06 23:11:50 +0800

    modify state_joint_action_distribution.py

--- RD_MARL/SCRD/state_joint_action_distribution.py
+++ RD_MARL/SCRD/state_joint_action_distribution.py
@@ -3,7 +3,7 @@ from scipy.linalg import solve
 from game_env import *
 
 
-def param_start_end(s_start, s_end, a_l, pi):
+def param_start_end(s_start, s_end, a_l, pi): # solve the Q value
     r_sum = 0
     for a_x in a_l:
         for a_y in a_l:
@@ -17,8 +17,25 @@ def param_matrix(s_l, a_l, pi):
         q_m.append([])
         for s_end in s_l:
             q_m[s_start].append(param_start_end(s_start, s_end, a_l, pi))
-    p_m = []
-    for s_start
+    return q_m
 
 
-def param_matrix
+def gen_s_joint_action_dist(s_l, a_l, s, a_x, a_y, pi):
+    param_m = param_matrix(s_l, a_l, pi)
+    a = np.array([[transition_prob(s, 1-s, a_x, a_y), param_m[1-s][1-s] - 1], [1, 1]])
+    # a = np.array([[transition_prob(s, s, a_x, a_y) - 1, param_m[1-s][s]], [1, 1]])
+    b = np.array([0, 1])
+    x = solve(a, b)
+    return x
+
+
+if __name__ == "__main__":
+    policy_pi = [{0: [0.2, 0.8], 1: [0.7, 0.3]}, {0: [0.7, 0.3], 1: [0.2, 0.8]}]
+    states = [0, 1]
+    actions = [0, 1]
+    state_dist = []
+    for s in states:
+        for a_x in actions:
+            for a_y in actions:
+                state_dist.append(gen_s_joint_action_dist(states, actions, s, a_x, a_y, policy_pi))
+    print(state_dist)
