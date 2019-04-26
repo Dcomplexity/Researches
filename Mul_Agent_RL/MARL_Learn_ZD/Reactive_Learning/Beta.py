0b1b79bbc72957da9e58db872006e21f399fed73
CHUANG DENG NEW MAC <cdengcnc@sjtu.edu.cn>
2019-04-26 23:39:09 +0800

    reactive learning

---
+++ Mul_Agent_RL/MARL_Learn_ZD/Reactive_Learning/Beta.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+a = np.random.beta(0.5, 0.5, 4)
+
+print(a)
---
+++ Mul_Agent_RL/MARL_Learn_ZD/Reactive_Learning/mem_one_strategy.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+p = [0.7881, 0.8888, 0.4686, 0.0792]
+q = [0.5, 0.5, 0.5, 0.5]
+tm = [[0.0 for i in range(4)] for j in range(4)]
+state = []
+for i in range(2):
+    for j in range(2):
+        state.append([i,j])
+for i in state:
+
+
+
