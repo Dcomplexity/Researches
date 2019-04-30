c7acae26271be9fcf6b9a706ce4e7e83c4444076
CHUANG DENG <cdengcnc@sjtu.edu.cn>
2019-04-30 18:57:14 +0800

    plt.savefig()

--- Mul_Agent_RL/MARL_Learn_ZD/Reactive_Learning/mem_one_strategy.py
+++ Mul_Agent_RL/MARL_Learn_ZD/Reactive_Learning/mem_one_strategy.py
@@ -45,6 +45,7 @@ if __name__ == '__main__':
     plt.xlim(left=0, right=5)
     plt.ylim(bottom=0, top=5)
     plt.plot(payoff_pair[:, 1], payoff_pair[:, 0], 'ro')
+    plt.savefig("mem_one_strategy.png")
     plt.show()
 
 
