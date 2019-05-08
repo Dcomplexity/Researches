00dddb940afe9ea21d9c7bc6df771c3b68219f04
CHUANG DENG <cdengcnc@sjtu.edu.cn>
2019-05-08 11:42:47 +0800

    reactive learn

--- Mul_Agent_RL/MARL_Learn_ZD/Reactive_Learning/test_files/reactive_learn_strategy_test.py
+++ Mul_Agent_RL/MARL_Learn_ZD/Reactive_Learning/test_files/reactive_learn_strategy_test.py
@@ -1,5 +1,4 @@
 import numpy as np
-import random
 import matplotlib.pyplot as plt
 
 
@@ -23,13 +22,18 @@ def choose_action(s):
     else:
         return 1
 
-
-def reactive_learning(s_p, s_q, r, s, t, p):
+if __name__ == '__main__':
+    s_p = [0.50, 0.99, 0.40, 0.01, 0.01]
+    s_q = [0.25, 0.25, 0.25, 0.25, 0.25]
+    r = 3
+    s = 0
+    t = 5
+    p = 1
     a_p = 0
     a_q = 0
     delta_p = 0.0
     p_list = []
-    for i in range(10000):
+    for i in range(5000):
         if i == 0:
             a_p = choose_action(s_p[0])
             a_q = choose_action(s_q[0])
@@ -39,7 +43,7 @@ def reactive_learning(s_p, s_q, r, s, t, p):
         else:
             a_p_last = a_p
             a_q_last = a_q
-            if a_q_last == 0:
+            if a_q == 0:
                 delta_p = delta_p * s_p[1] + (1 - delta_p) * s_p[3]
             else:
                 delta_p = delta_p * s_p[2] + (1 - delta_p) * s_p[4]
@@ -47,24 +51,10 @@ def reactive_learning(s_p, s_q, r, s, t, p):
             a_q = choose_action(s_q[a_q_last * 2 + a_p_last + 1])
             payoff_p, payoff_q = play_game(a_p, a_q, r, s, t, p)
             p_list.append([payoff_p, payoff_q])
-    return np.array(p_list).mean(axis=0)
+
+p_list = np.array(p_list)
+print(p_list.mean(axis=0))
 
 
-if __name__ == '__main__':
-    s_p_r = [0.50, 0.99, 0.40, 0.01, 0.01]
-    # s_q_r = [0.25, 0.25, 0.25, 0.25, 0.25]
-    r_r = 3
-    s_r = 0
-    t_r = 5
-    p_r = 1
-    payoff_pair = []
-    for _ in np.arange(10e5):
-        print(_)
-        s_q_r = np.random.beta(0.5, 0.5, 5)
-        payoff_pair.append(reactive_learning(s_p_r, s_q_r, r_r, s_r, t_r, p_r))
-    payoff_pair = np.array(payoff_pair)
-    plt.xlim(left = 0, right = 5)
-    plt.ylim(bottom = 0, top = 5)
-    plt.scatter(payoff_pair[:, 1], payoff_pair[:, 0], s-2.0, c='green')
-    plt.savefig('./images/reactive_learn_strategy.png')
-    plt.show()
+
+
